65a320906233845de60bbe29deec0df6769a00e4
colesbury <colesbury@db329023-cf4e-0410-aa6c-69754c57d2e5>
2009-06-25 05:23:55 +0000

    Updated copyright header.

--- src/PluginInstanceOpenWithFinder.mm
+++ src/PluginInstanceOpenWithFinder.mm
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2005, 2006, 2007 Apple Inc. All rights reserved.
- * Copyright (c) 2008 Samuel Gross.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -25,6 +24,10 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Modified by Sam Gross <colesbury@gmail.com> for use with Firefox PDF Plugin for Mac OS X.
  */
 #import "PluginInstance.h"
 #import <unistd.h>
